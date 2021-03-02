2fd9e2f17c927e5c53d77948191e0c28897b53d8
Fastus <fastus6666@gamil.com>
2021-03-02 10:18:58 +0100

    First commit

--- src/main/java/pl/fastus/spacestation/SpaceStationLocalizerApplication.java
+++ src/main/java/pl/fastus/spacestation/SpaceStationLocalizerApplication.java
@@ -1,13 +1,13 @@
-package pl.fastus.issspacestationlocalizer;
+package pl.fastus.spacestation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IssSpaceStationLocalizerApplication {
+public class SpaceStationLocalizerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IssSpaceStationLocalizerApplication.class, args);
+		SpringApplication.run( SpaceStationLocalizerApplication.class, args);
 	}
 
 }
--- src/test/java/pl/fastus/spacestation/SpaceStationLocalizerApplicationTests.java
+++ src/test/java/pl/fastus/spacestation/SpaceStationLocalizerApplicationTests.java
@@ -1,10 +1,10 @@
-package pl.fastus.issspacestationlocalizer;
+package pl.fastus.spacestation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IssSpaceStationLocalizerApplicationTests {
+class SpaceStationLocalizerApplicationTests {
 
 	@Test
 	void contextLoads() {
