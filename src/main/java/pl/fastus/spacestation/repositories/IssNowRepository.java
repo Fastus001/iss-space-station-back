d4bd43a94e9284ce55efc6ce54780cc018a5467c
Fastus <fastus6666@gamil.com>
2021-03-02 13:57:25 +0100

    Create IssNow repository. Closes #2.

---
+++ src/main/java/pl/fastus/spacestation/repositories/IssNowRepository.java
@@ -0,0 +1,8 @@
+package pl.fastus.spacestation.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import pl.fastus.spacestation.domain.IssNow;
+
+public interface IssNowRepository extends CrudRepository<IssNow, Long> {
+
+}
