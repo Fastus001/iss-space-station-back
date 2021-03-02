d49190af7e22cf81d7e5825815c162414df9ff4a
Fastus <fastus6666@gamil.com>
2021-03-02 10:10:49 +0100

    Frist commit.

---
+++ src/test/java/pl/fastus/issspacestationlocalizer/IssSpaceStationLocalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fastus.issspacestationlocalizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssSpaceStationLocalizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
