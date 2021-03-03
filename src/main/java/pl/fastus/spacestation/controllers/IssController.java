fad2798742ed0762d973e9de0e656a89003fceba
Fastus <fastus6666@gamil.com>
2021-03-03 15:11:06 +0100

    Addes map display to show.html and pass IssNow to model attribute. Closes #17.

--- src/main/java/pl/fastus/spacestation/controllers/IssController.java
+++ src/main/java/pl/fastus/spacestation/controllers/IssController.java
@@ -1,13 +1,22 @@
 package pl.fastus.spacestation.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.fastus.spacestation.services.OkHttpService;
 
 @Controller
 public class IssController {
 
+    private final OkHttpService okHttpService;
+
+    public IssController(OkHttpService okHttpService) {
+        this.okHttpService = okHttpService;
+    }
+
     @RequestMapping({"iss/show", "iss/showlocation"})
-    public String show(){
+    public String show(Model model){
+        model.addAttribute( "issNow", okHttpService.getIssNow());
         return "iss/show";
     }
 }
