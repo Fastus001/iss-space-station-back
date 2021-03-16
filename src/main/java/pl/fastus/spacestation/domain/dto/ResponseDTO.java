e7490aff5163d4ce297d4197a07e48316f1fa972
Fastus <fastus6666@gamil.com>
2021-03-16 15:00:48 +0100

    Created tests for IssServiceImpl. Closes #38.
    Probably can be done much cleaner.

--- src/main/java/pl/fastus/spacestation/domain/dto/ResponseDTO.java
+++ src/main/java/pl/fastus/spacestation/domain/dto/ResponseDTO.java
@@ -2,10 +2,12 @@
 package pl.fastus.spacestation.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class ResponseDTO {
 
--- src/test/java/pl/fastus/spacestation/services/IssApiServiceImplTest.java
+++ src/test/java/pl/fastus/spacestation/services/IssApiServiceImplTest.java
@@ -1,35 +1,100 @@
 package pl.fastus.spacestation.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import pl.fastus.spacestation.domain.IssPassesRequest;
+import pl.fastus.spacestation.domain.StationNow;
+import pl.fastus.spacestation.domain.dto.*;
 
-@ExtendWith(MockitoExtension.class)
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class IssApiServiceImplTest {
-    private static final int NUMBER_OF_PASSES = 10;
 
-    @Mock
-    RestTemplate restTemplate;
+    public static MockWebServer mockWebServer;
 
-    @InjectMocks
     IssApiApiServiceImpl service;
 
+    ObjectMapper objectMapper;
 
+    @BeforeAll
+    static void setUp() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        mockWebServer.shutdown();
+    }
 
     @BeforeEach
     void setService(){
-//        service = new OkHttpServiceImpl(restTemplate, requestConverter, responseConverter);
+        String baseUrl =  String.format( "http://localhost:%s", mockWebServer.getPort() );
+        service = new IssApiApiServiceImpl( baseUrl, baseUrl );
+        objectMapper = new ObjectMapper();
     }
 
     @Test
-    void getIssNow() {
-//        final IssNow issNow = service.getIssNow();
+    void getIssNowTest() throws JsonProcessingException {
+        IssPosition position= new IssPosition(25.25,20.20);
+        StationNowDTO mockStationNowDTO = new StationNowDTO(position, "message",123456L);
 
-//        assertNotNull( issNow );
+        mockWebServer.enqueue( new MockResponse().setBody( objectMapper.writeValueAsString( mockStationNowDTO ) )
+                                        .addHeader( "Content-Type", "application/json" ));
+
+        final StationNow issNow = service.getIssNow();
+
+        System.out.println( "issNow = " + issNow );
+        assertNotNull(issNow);
+        assertEquals( 25.25, issNow.getPosition().getLatitude() );
+        assertEquals( 123456, issNow.getTimeStamp() );
+    }
+
+    @Test
+    void createIssPassesRequestTest() throws JsonProcessingException, InterruptedException {
+        RequestDTO requestDTO = new RequestDTO(25.20,25.20,15,2,123456L);
+
+        ResponseDTO response1 = new ResponseDTO(456789L,456 );
+        ResponseDTO response2 = new ResponseDTO(1234667L,999 );
+
+        StationPassesRequestDTO passesRequestDTO = new StationPassesRequestDTO();
+        passesRequestDTO.setMessage( "message" );
+        passesRequestDTO.setRequestDTO( requestDTO );
+        passesRequestDTO.setResponseDTO( List.of(response1, response2) );
+
+        mockWebServer.enqueue( new MockResponse().setBody( objectMapper.writeValueAsString( passesRequestDTO ) )
+                                       .addHeader( "Content-Type", "application/json" ));
+
+        final IssPassesRequest block = service.createIssPassesRequest( getValueMap() ).block();
+
+
+        assertNotNull(block);
+        assertEquals( 2, block.getResponses().size() );
+        assertEquals( 25.20, block.getLatitude());
+        assertEquals( 15, block.getAltitude());
+    }
+
+    @NotNull
+    private MultiValueMap<String, String> getValueMap() {
+        MultiValueMap<String, String> multiValueMap = new LinkedMultiValueMap<>();
+        multiValueMap.add( "lat", "25.20" );
+        multiValueMap.add( "lon", "25.20" );
+        multiValueMap.add( "alt", "15" );
+        multiValueMap.add( "n", "2" );
+        return multiValueMap;
     }
 
 }
