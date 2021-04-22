b990f53fc7487372c71c731bfe81c6ec66d34dfe
Fastus <fastus6666@gamil.com>
2021-04-22 15:57:24 +0200

    Refactor ISS Controller to be as REST api endpoints. #47
    removed not needed IssPasses Repository and service.

--- src/main/java/pl/fastus/spacestation/repositories/IssPassesRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.fastus.spacestation.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import pl.fastus.spacestation.domain.IssPass;
-
-public interface IssPassesRepository extends CrudRepository<IssPass, Long> {
-
-}
